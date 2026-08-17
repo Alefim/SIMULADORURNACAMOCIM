d373086bc5b7f4f91280b6a132423a7736bada81
alefim.dev <tec.alefoliveira@gmail.com>
2026-08-17 20:24:13 -0300

    Remove registro antigo de service worker

--- js/script.js
+++ js/script.js
@@ -161,15 +161,3 @@ function confirma() {
 }
 
 comecarEtapa();
-
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('js/service-worker.js')
-            .then((registration) => {
-                console.log('ServiceWorker registrado com sucesso:', registration);
-            })
-            .catch((error) => {
-                console.log('Falha ao registrar o ServiceWorker:', error);
-            });
-    });
-}
