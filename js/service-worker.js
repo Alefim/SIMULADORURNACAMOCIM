2f8203e77ee023357c5f2e05e1d0ba763a10f28d
alefim.dev <tec.alefoliveira@gmail.com>
2026-08-17 20:13:35 -0300

    Atualiza cache da PWA para buscar arquivos recentes

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,28 +1,42 @@
-const CACHE_NAME = 'pwa-cache-v1';
-const urlsToCache = [
+const CACHE_NAME = 'pwa-cache-v3';
+const CORE_ASSETS = [
   '/',
-  'index.html',
-  'css/style.css',
-  'js/script.js',
-  'js/etapas.js',
-  'icons/192x192.png',
-  'icons/512x512.png'
+  '/index.html',
+  '/css/style.css?v=20260817-2015'
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(CORE_ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      ))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+
   event.respondWith(
-    caches.match(event.request)
+    fetch(event.request)
       .then((response) => {
-        return response || fetch(event.request);
+        const copia = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, copia);
+        });
+        return response;
       })
+      .catch(() => caches.match(event.request))
   );
 });
