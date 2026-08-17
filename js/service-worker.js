6e7634c1c2b91586c3d8a8d86be4f30ed9a66ce8
alefim.dev <tec.alefoliveira@gmail.com>
2026-08-17 20:24:37 -0300

    Desativa service worker antigo de js

--- js/service-worker.js
+++ js/service-worker.js
@@ -1,42 +1,13 @@
-const CACHE_NAME = 'pwa-cache-v3';
-const CORE_ASSETS = [
-  '/',
-  '/index.html',
-  '/css/style.css?v=20260817-2015'
-];
-
-self.addEventListener('install', (event) => {
+self.addEventListener('install', () => {
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(CORE_ASSETS))
-  );
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys()
-      .then((keys) => Promise.all(
-        keys
-          .filter((key) => key !== CACHE_NAME)
-          .map((key) => caches.delete(key))
-      ))
-      .then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    fetch(event.request)
-      .then((response) => {
-        const copia = response.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, copia);
-        });
-        return response;
-      })
-      .catch(() => caches.match(event.request))
-  );
+  event.waitUntil((async () => {
+    const cacheNames = await caches.keys();
+    await Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({ type: 'window' });
+    clients.forEach((client) => client.navigate(client.url));
+  })());
 });
