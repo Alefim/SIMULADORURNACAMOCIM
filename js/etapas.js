f2f9a7b2ebfdcd1b3e4c202e1d82dc1a0f06fab7
alefim.dev <tec.alefoliveira@gmail.com>
2026-08-26 21:16:51 -0300

    Altera ordem da votação: federal antes de estadual

--- js/etapas.js
+++ js/etapas.js
@@ -1,5 +1,23 @@
 let etapas = [
     {
+        titulo: 'DEPUTADO FEDERAL',
+        numeros: 4,
+        candidatos: [
+            {
+                numero: '4044',
+                nome: 'ROGER AGUIAR',
+                partido: 'PSB',
+                fotos: [{ url: 'rogeraguiar.jpg', legenda: 'Candidata' }]
+            },
+            {
+                numero: '4077',
+                nome: 'TAINAH MARINHO',
+                partido: 'PSB',
+                fotos: [{ url: 'tainahmarinho.jpg', legenda: 'Candidata' }]
+            }
+        ]
+    },
+    {
         titulo: 'DEPUTADO ESTADUAL',
         numeros: 5,
         candidatos: [
@@ -20,24 +38,6 @@ let etapas = [
                 nome: 'EUVALDETE FERRO',
                 partido: 'PSDB',
                 fotos: [{ url: 'euvaldete.jpg', legenda: 'Candidata' }]
-            }
-        ]
-    },
-    {
-        titulo: 'DEPUTADO FEDERAL',
-        numeros: 4,
-        candidatos: [
-            {
-                numero: '4044',
-                nome: 'ROGER AGUIAR',
-                partido: 'PSB',
-                fotos: [{ url: 'rogeraguiar.jpg', legenda: 'Candidata' }]
-            },
-            {
-                numero: '4077',
-                nome: 'TAINAH MARINHO',
-                partido: 'PSB',
-                fotos: [{ url: 'tainahmarinho.jpg', legenda: 'Candidata' }]
             }
         ]
     }
